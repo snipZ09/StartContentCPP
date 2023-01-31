64514feb4cafd65547b211667796d8ba4a22b379
HieuTu <hieu.tu@alta.com.vn>
2023-01-31 10:06:10 +0700

    feature: create class combat character

---
+++ Source/StartContentCPP/Characters/CombatCharacter.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombatCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+// Sets default values
+ACombatCharacter::ACombatCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Camera Boom
+	//CreateDefaultSubobject<sub obj thuộc class nào>(Text tên của CameraBoom hiện trên editor)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	//FollowCamera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+// Called to bind functionality to input
+void ACombatCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ACombatCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
